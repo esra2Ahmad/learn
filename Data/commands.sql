d1b1c0c1a64fd981cdaaaefd2c6ab3a13ede3e19
31211995 <esra2.yossef@gmail.com>
2024-08-21 17:56:11 +0400

    first commit

--- Data/commands.sql
+++ Data/commands.sql
@@ -1,3 +1,5 @@
 -- SQLite
 create table if not exists contact_form(ID_integer primary key,Name text not null,
                                             Email text not null unique, Message text not null, Created_Data text not null)
+                                            SELECT * FROM sqlite_master where type="table";
+                                            select * from contact_form;
